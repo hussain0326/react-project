13bdb77e0b334a6a2bb2c2dde30067aa98ce0167
Hussain <hussainbigzad@gmail.com>
2022-12-23 13:55:48 +0100

    fix:test error

--- src/setupTest.js
+++ src/setupTest.js
@@ -1,5 +1,6 @@
 import Enzyme, { shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@testing-library/jest-dom';
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 // Make Enzyme functions available in all test files without importing
